6d64dcaff1dba32618bde840c8021fb660c57346
vitor29393dj-tech <vitor29393dj@gmail.com>
2026-04-16 22:11:57 -0300

    Update NavigationController.php

--- src/controllers/NavigationController.php
+++ src/controllers/NavigationController.php
@@ -1,1 +1,11 @@
+<?php
+// NavigationController.php
+// Este arquivo funciona como o "recepcionista" do sistema.
 
+/* Logica de Redirecionamento:
+Se o usuário clicar em 'Cadastrar', o Controller valida os dados 
+através do Model e, se estiver tudo certo, chama a View de 'Sucesso'.
+*/
+
+echo "Controller de Navegação Ativo";
+?>
