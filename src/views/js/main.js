f28ba9e00b1111d53490b93384ff29ac3dd030cf
vitor29393dj-tech <vitor29393dj@gmail.com>
2026-04-16 22:13:56 -0300

    Update main.js

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -1,1 +1,9 @@
+// main.js - Lógica de interatividade do Front-end
 
+function feedbackAcaoPrincipal() {
+    // Exibe um alerta como feedback de UX
+    alert("Ação realizada com sucesso! (Feedback Visual)");
+    
+    // Altera a cor de fundo para confirmar a ação ao usuário
+    document.body.style.backgroundColor = "#d4edda"; 
+}
